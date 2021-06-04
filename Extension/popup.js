c749c1271bff9ccda8dfb5453a17af63d3a0f122
GoncalvesDanilo <danilo.in.development@gmail.com>
2021-06-03 21:01:52 -0300

    ✨ Copy emoji to clipboard

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,27 +1,39 @@
 let content = document.getElementById('list');
 
+const copy = (className) => {
+  const emojiName = className.split(' ')[1];
+  const emoji = gitmojis.find(emoji => emoji.name === emojiName).emoji;
+  navigator.clipboard.writeText(emoji)
+  .then(() => {
+    console.log('Copied!');
+  })
+  .catch((error) => {
+    alert(error);
+  });
+};
+
 gitmojis.forEach((gitmoji) => {
   let item = document.createElement('div');
-  item.className = 'list-item';
-  item.setAttribute('style', `--item-color:${gitmoji.color}`)
-  
+  item.className = 'list-item ' + gitmoji.name;
+  item.setAttribute('style', `--item-color:${gitmoji.color}`);
+  item.addEventListener('click', (click) => copy(click.target.className));
+
   let emoji = document.createElement('div');
-  emoji.className = 'item-emoji';
+  emoji.className = 'item-emoji ' + gitmoji.name;
   emoji.innerText = gitmoji.emoji;
-  item.contentEditable = true;
   item.appendChild(emoji);
 
   let textWrap = document.createElement('div');
-  textWrap.className = 'item-text-wrap';
+  textWrap.className = 'item-text-wrap ' + gitmoji.name;
   item.appendChild(textWrap);
 
   let code = document.createElement('div');
-  code.className = 'item-code';
+  code.className = 'item-code ' + gitmoji.name;
   code.innerText = gitmoji.code;
   textWrap.appendChild(code);
 
   let description = document.createElement('div');
-  description.className = 'item-description';
+  description.className = 'item-description ' + gitmoji.name;
   description.innerText = gitmoji.description;
   textWrap.appendChild(description);
 
